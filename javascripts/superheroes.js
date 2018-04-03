488b0aea7c693e4dfd22d68bcc34daa47adce0ee
Lola Simpson <lolalsimp@gmail.com>
2018-04-02 23:07:25 -0500

    added XHR on superheroes.js

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -1,1 +1,34 @@
-console.log('Superheroes');
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+}
+
+
+const buildDomString = (theArray) => {
+    let domString = "";
+    theArray.forEach((superheroes) => {
+        domString += `<div class="hero">`;
+        domString +=`<h2>${superheroes.name}</h2>`;
+        domString += `</div>`;
+    })
+    printToDom(domString, "all-hero");
+};
+
+function executeThisCodeAfterLoaded(){
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.superheroes);
+}
+
+function executeThisCodeifXHRFails(){
+    console.log("NOPE!!!");
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeThisCodeAfterLoaded);
+    myRequest.addEventListener('error', executeThisCodeifXHRFails);
+    myRequest.open("GET", "../db/superhoes.json");
+    myRequest.send();
+
+}
+
+startApplication();
