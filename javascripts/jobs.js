d95d69e4f08a715ee4b014df09fbce2819ea6252
Lola Simpson <lolalsimp@gmail.com>
2018-04-03 20:05:19 -0500

    added dropdown button

--- javascripts/jobs.js
+++ javascripts/jobs.js
@@ -1,1 +1,34 @@
-console.log('Jobs');
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+}
+
+
+const buildDomString = (theArray) => {
+    let domString = "";
+    theArray.forEach((superheroes) => {
+        domString += `<li>`;
+        domString += `<a href="#" data-hero-id="${superheroes.id}"> ${superheroes.name}</a>`;
+        domString += `</li>`;
+    })
+    printToDom(domString, "awesome-dropdown");
+};
+
+function executeThisCodeAfterLoaded(){
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.superheroes);
+}
+
+function executeThisCodeifXHRFails(){
+    console.log("NOPE!!!");
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeThisCodeAfterLoaded);
+    myRequest.addEventListener('error', executeThisCodeifXHRFails);
+    myRequest.open("GET", "../db/superhoes.json");
+    myRequest.send();
+
+}
+
+startApplication();
